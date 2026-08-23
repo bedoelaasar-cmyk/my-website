7b2fe5e6e6ec304a8044ab1223fe645101a6c507
bedoelaasar-cmyk <bedoelaasar@gmail.com>
2026-08-23 20:24:15 +0300

    Add files via upload

--- db.js
+++ db.js
@@ -1,16 +1,20 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
-const connection = mysql.createConnection(
-    process.env.MYSQL_url
-);
+const connection = mysql.createConnection({
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE
+});
 
 connection.connect((err) => {
     if (err) {
-        console.log('Error connecting to MySQL:', err);
+        console.log("Error connecting to MySQL:", err);
         return;
     }
 
-    console.log('Connected to MySQL successfully!');
+    console.log("Connected to MySQL successfully!");
 });
 
 module.exports = connection;
