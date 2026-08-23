40181f82355fc23c495b3a87fd0e836c28137c20
bedoelaasar-cmyk <bedoelaasar@gmail.com>
2026-08-23 19:49:25 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -73,6 +73,8 @@ app.post("/login", async (req, res) => {
     );
 });
 
-app.listen(3000, () => {
-    console.log("Website is running at http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Website is running on port ${PORT}`);
 });
