938f8701ad2f9353206bc38dff29a2d900899ae6
bedoelaasar-cmyk <bedoelaasar@gmail.com>
2026-08-23 20:17:28 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,11 +1,8 @@
 const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Abdo@2008',
-    database: 'mywebsite'
-});
+const connection = mysql.createConnection(
+    process.env.MYSQL_url
+);
 
 connection.connect((err) => {
     if (err) {
